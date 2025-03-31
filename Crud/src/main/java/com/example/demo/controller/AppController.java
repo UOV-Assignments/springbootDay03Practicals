9c6facfe17b2ecead77efeaf95a993902ef1245d
lasanga890 <lasangad890@gmail.com>
2025-04-01 00:07:15 +0530

    `Added new method to sort students by GPA in descending order using bubble sort algorithm.`

--- Crud/src/main/java/com/example/demo/controller/AppController.java
+++ Crud/src/main/java/com/example/demo/controller/AppController.java
@@ -62,5 +62,21 @@ public class AppController {
 		}
 		return range;
 	}
+	
+	@GetMapping("/student/sorted-by-gpa")
+    public List<Student> getStudentsSortedByGPA() {
+
+        int n = students.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (students.get(j).getGpa() > students.get(j + 1).getGpa()) {
+                    Student temp = students.get(j);
+                    students.set(j, students.get(j + 1));
+                    students.set(j + 1, temp);
+                }
+            }
+        }
+        return students;
+    }
 
 }
